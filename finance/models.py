31082cdf93da5226ff5504acc9c68974bf779cc2
Nunes <nunes.guicardoso@gmail.com>
2026-02-14 23:23:36 +0000

    minor add

--- finance/models.py
+++ finance/models.py
@@ -96,6 +96,8 @@ class PortfolioSnapshot(models.Model):
     currency = models.CharField(max_length=20)
     amount = models.DecimalField(max_digits=20, decimal_places=8)
     average_price = models.DecimalField(max_digits=20, decimal_places=8)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"{self.currency} - {self.amount} unidades a {self.average_price} cada"
