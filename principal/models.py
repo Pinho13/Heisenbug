e48dcb94a98b13d4aecd6680b016f22e3b5bb1dc
AugustoVSoares <dacbvs@gmail.com>
2026-02-14 14:32:14 +0000

    feat: functions to add/edit users to allow for easier access from front end

--- principal/models.py
+++ principal/models.py
@@ -1,8 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from rest_framework import serializers
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+    REQUIRED_FIELDS = ['username']
 
 
 class User(AbstractUser):
+    objects = CustomUserManager()
     verified = models.BooleanField(default=False)
     crypto_wallet_address = models.CharField(
         max_length=255, blank=True, null=True)
@@ -23,3 +43,17 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'bio']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
