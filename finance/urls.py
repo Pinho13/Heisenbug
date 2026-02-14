289a3d85f94c32a0197d1bbbcad6e758bc538246
Nunes <nunes.guicardoso@gmail.com>
2026-02-14 08:59:03 +0000

    urls

---
+++ finance/urls.py
@@ -0,0 +1,7 @@
+from django import path
+from . import views
+
+urlpatterns = [
+    path('ticker/btc/', views.btc_usd_price, name='btc_price'),
+    path('ticker/<str:pair>/', views.price_view, name='price_view'),
+]
