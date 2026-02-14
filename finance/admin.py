2436e3a1e311d317da6d9219af445059f49b0544
Nunes <nunes.guicardoso@gmail.com>
2026-02-14 10:29:18 +0000

    classes

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import AssetPool, TradeHistory
 
-# Register your models here.
+@admin.register(AssetPool)
+class AssetPoolAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'user', 'is_active', 'target_allocation')
+    list_filter = ('is_active', 'user')
+
+@admin.register(TradeHistory)
+class TradeHistoryAdmin(admin.ModelAdmin):
+    list_display = ('pair', 'operation', 'amount', 'price_at_execution', 'timestamp')
+    readonly_fields = ('timestamp',) # Impede que alteres a hora manualmente
