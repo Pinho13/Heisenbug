18d8d09e287e30e34bc51d9c364da63afb08d8be
Pinho <rafaelpinhotsilva@gmail.com>
2026-02-14 02:34:24 +0000

    Fixes

--- core/urls.py
+++ core/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from finance.views import price_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # dynamic URL (:fire:)
     path('price/<str:pair>/', price_view, name= 'price-view'),
+    path('', include('principal.urls')),
 ]
